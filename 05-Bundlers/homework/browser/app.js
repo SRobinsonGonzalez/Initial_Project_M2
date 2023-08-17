65e33410fbbbd6371739b4d44e5cc8dc9313b4c5
RobinsonG <hh.robinson95@hotmail.com>
2023-08-17 14:26:54 -0500

    Homework Bundlers

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,6 +1,6 @@
-(function () {
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+  var whiteboard = require('./whiteboard');
+  const io = require('socket.io-client');
+  var socket = io(window.location.origin);
 
   socket.on("connect", function () {
     console.log("Connected!");
@@ -22,4 +22,3 @@
   whiteboard.on("draw", function (start, end, color) {
     socket.emit("draw", start, end, color);
   });
-})();
--- 05-Bundlers/homework/browser/whiteboard.js
+++ 05-Bundlers/homework/browser/whiteboard.js
@@ -1,5 +1,5 @@
-(function () {
-  window.whiteboard = new window.EventEmitter();
+  const EventEmitter = require('./event-emitter');
+  const whiteboard = new EventEmitter();
 
   // Ultimately, the color of our stroke;
   var color;
@@ -106,4 +106,5 @@
       whiteboard.emit("draw", start, end, strokeColor);
     }
   };
-})();
+
+  module.exports = whiteboard;
