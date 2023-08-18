339435956554745410da86c5c72fc5a1ea7eefa7
RobinsonG <hh.robinson95@hotmail.com>
2023-08-18 15:11:26 -0500

    Start of Integration

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,13 +1,14 @@
 export default function Card(props) {
    return (
       <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+         <button onClick={props.onClose}>X</button>
+         <h2>Name: {props.name}</h2>
+         <h2>Status: {props.status}</h2>
+         <h2>Species: {props.species}</h2>
+         <h2>Gender: {props.gender}</h2>
+         <h2>Origin: {props.origin}</h2>
+         <img src={props.image} alt={props.name} />
+         {/* <img src='' alt={`Imagen no encontrada de ${props.name}`} /> */}
       </div>
    );
 }
